1f408af4e0e24ee334885e23ff3de60e67b6dd14
Prasetyo W <prasetyowls12@yahoo.com>
2019-10-11 18:18:43 +0700

    Trying mosca server, use index2.js to run server

---
+++ UI/index2.js
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------
+
+// Setup Server
+var brokerAddress = "localhost";
+var brokerPort = 1884;
+var httpAddress = "localhost";
+var httpPort = 3003;
+
+// Embedded Mosca initialization
+var mosca = require("mosca");
+var server = new mosca.Server({
+  // host: brokerAddress,
+  port: brokerPort,
+  http: {
+    // host: httpAddress,
+    port: httpPort,
+    bundle: true,
+    static: './public/'
+  }
+});
+
+// Triggered when server status is ready
+server.on('ready', function(){
+    console.log('-------------------------------')
+    console.log('Mosca Broker is up and running in %s:%s !', brokerAddress, brokerPort)
+    console.log('Using %s:%s for HTTP and MQTT over Web-Sockets !', httpAddress, httpPort)
+    console.log('-------------------------------')
+    console.log('')
+})
+
+// Triggered when a client is connected
+server.on('clientConnected', function(client) {
+    console.log('BROKER : client connected (%s)', client.id)
+})
+
+server.on('clientDisconnected', function(client) {
+    console.log('BROKER : client disconnected (%s)', client.id)
+})
+
+// Triggered when a message is received
+server.on('published', function(packet, client) {
+    console.log('MESSAGE : %s', packet.payload.toString('utf-8'))
+})
+
+//---------------------------------------------------------------
--- UI/public/js/script.js
+++ UI/public/js/script.js
@@ -1,211 +1,193 @@
-var s1 = 0
-var s2 = 0
-var s3 = 0
-var s4 = 0
+//  Tweakables
+var maxData = 25;
 
-var maxData = 100;
+// Global Variables
+var ledstate = [false, false, false, false]
 var dataDump = [];
-const host = 'http://localhost:3003'
 
-async function changeValue() {
-  var response = await fetch(host + '/messageIn') // AJAX - to asynchronously fetch / "get" data
-  var data = await response.json()
+// ----------------------------------------------------------------------------
 
-  // Update HTML content
-  // console.log('received data: %s', data.value1);
-  document.getElementById("humidity_value").innerHTML = parseFloat(data.value1).toFixed(2);
-  document.getElementById("temperature_value").innerHTML = parseFloat(data.value2).toFixed(2);
-  document.getElementById("brightness_value").innerHTML = parseFloat(data.value3).toFixed(2);
-  // document.getElementById("hum_gauge").style.marginLeft = data.value1 + "%";
+// MQTT Setup
+var client = mqtt.connect();
 
-  // dataDump.push([d,data]); // --> To save it into database later, commented untuk menghemat memori hehe
+client.on('connect', function() {
+    client.subscribe('topic/sensor1')
+    client.subscribe('topic/sensor2')
+    client.subscribe('topic/sensor3')
+    client.subscribe('topic/ledstatus1')
+    client.subscribe('topic/ledstatus2')
+    client.subscribe('topic/ledstatus3')
+    client.subscribe('topic/ledstatus4')
+})
 
-  // Update chart
-  d = new Date();
-  if (mychart.data.datasets[0].data.length > maxData) { //Limit number of displayed data at one instance
-    mychart.data.labels.shift();
-    mychart.data.datasets[0].data.shift();
-    mychart.data.datasets[1].data.shift();
-    mychart.data.labels.push(d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
-    mychart.data.datasets[0].data.push(data.value1);
-    mychart.data.datasets[1].data.push(data.value2);
-  } else {
-    mychart.data.labels.push(d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
-    mychart.data.datasets[0].data.push(data.value1);
-    mychart.data.datasets[1].data.push(data.value2);
-  }
-
-  mychart.update();
-
-  gauge.set(data.value1 * 100);
-  document.getElementById("circle").style.marginLeft = (data.value3 / 1100 * 100) + "%";
-  bar1.set(data.value2 / 30 * 100);
-}
-
-var x1 = false;
-var x2 = false;
-var x3 = false;
-var x4 = false;
-
-function changeStatus1() {
-  x1 = !x1;
-  switch (x1) {
-    case false: // LED mati
-      document.getElementById("ledstatus1").style.backgroundColor = "rgb(231, 76, 60)";
-      break;
-    case true: // LED nyala
-      document.getElementById("ledstatus1").style.backgroundColor = "rgb(46, 204, 113)";
-      break;
-    default: // Tidak ada data
-      document.getElementById("ledstatus1").style.backgroundColor = "white";
-  }
-}
-
-function changeStatus2() {
-  x2 = !x2;
-  switch (x2) {
-    case false: // LED mati
-      document.getElementById("ledstatus2").style.backgroundColor = "rgb(231, 76, 60)";
-      break;
-    case true: // LED nyala
-      document.getElementById("ledstatus2").style.backgroundColor = "rgb(46, 204, 113)";
-      break;
-    default: // Tidak ada data
-      document.getElementById("ledstatus2").style.backgroundColor = "white";
-  }
-}
-
-function changeStatus3() {
-  x3 = !x3;
-  switch (x3) {
-    case false: // LED mati
-      document.getElementById("ledstatus3").style.backgroundColor = "rgb(231, 76, 60)";
-      break;
-    case true: // LED nyala
-      document.getElementById("ledstatus3").style.backgroundColor = "rgb(46, 204, 113)";
-      break;
-    default: // Tidak ada data
-      document.getElementById("ledstatus3").style.backgroundColor = "white";
-  }
-}
-
-function changeStatus4() {
-  x4 = !x4;
-  switch (x4) {
-    case false: // LED mati
-      document.getElementById("ledstatus4").style.backgroundColor = "rgb(231, 76, 60)";
-      break;
-    case true: // LED nyala
-      document.getElementById("ledstatus4").style.backgroundColor = "rgb(46, 204, 113)";
-      break;
-    default: // Tidak ada data
-      document.getElementById("ledstatus4").style.backgroundColor = "white";
-  }
-}
-
-// Get Time
-var d = new Date()
-var h = d.getHours()
-var m = d.getMinutes()
-var s = (d.getMilliseconds()) / 1000
-var now = h + ':' + m + ':' + s
-
-// chart.js
-var ctx = document.getElementById('canvas').getContext('2d');
-
-var mydatas = {
-  labels: [],
-  datasets: [{
-    label: 'Humidity',
-    // backgroundColor: chartColors.grey,
-    borderColor: '#3cba9f',
-    data: [],
-    fill: false,
-    yAxisID: 'y-axis-1',
-  }, {
-    label: 'Temperature',
-    // backgroundColor: chartColors.blue,
-    borderColor: '#c45850',
-    data: [],
-    fill: false,
-    yAxisID: 'y-axis-2',
-  }]
-};
-
-var config = {
-  data: mydatas,
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    hoverMode: 'index',
-    stacked: false,
-    scales: {
-      yAxes: [{
-        type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-        display: true,
-        position: 'left',
-        id: 'y-axis-1',
-        gridLines: {
-          color: '#252525',
-        },
-      }, {
-        type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-        display: true,
-        position: 'right',
-        id: 'y-axis-2',
-
-        // grid line settings
-        gridLines: {
-          drawOnChartArea: false, // only want the grid lines for one axis to show up
-        },
-      }],
-      xAxes: [{
-        // type: 'time',
-        ticks: {
-          autoSkip: true,
-          maxTicksLimit: maxData / 2
-        },
-        gridLines: {
-          color: '#252525',
-        },
-      }],
+client.on('message', function(topic, message) {
+    //console.log('received message on %s: %s', topic, message)
+    switch (topic) {
+        case 'topic/sensor1':
+            changeValue(message, "humidity_value");
+            break;
+        case 'topic/sensor2':
+            changeValue(message, "temperature_value");
+            break;
+        case 'topic/sensor3':
+            changeValue(message, "brightness_value");
+            break;
+        case 'topic/ledstatus1':
+            changeLED(message, 1);
+            break;
+        case 'topic/ledstatus2':
+            changeLED(message, 2);
+            break;
+        case 'topic/ledstatus3':
+            changeLED(message, 3);
+            break;
+        case 'topic/ledstatus4':
+            changeLED(message, 4);
+            break;
     }
+})
+
+// ----------------------------------------------------------------------------
+
+function changeValue(value, value_id) {
+    // Update HTML content
+    //console.log('Received data VALUE for id %s : %s',value_id,value);
+    document.getElementById(value_id).innerHTML = value
+
+    // Update chart
+    switch (value_id) {
+        case 'humidity_value':
+            insertData(chart1,value);
+            gauge.set(value * 100);
+            break;
+        case 'temperature_value':
+            insertData(chart2,value);
+            bar1.set(value / 30 * 100);
+            break;
+        case 'brightness_value':
+            insertData(chart3,value);
+            document.getElementById("circle").style.marginLeft = (value / 1100 * 100) + "%";
+            break;
+    }
+}
+
+function insertData(chart_id, value) {
+  var d = new Date();
+  if (chart_id.data.datasets[0].data.length > maxData) { //Limit number of displayed data at one instance
+      chart_id.data.labels.shift();
+      chart_id.data.datasets[0].data.shift();
+      chart_id.data.labels.push(d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
+      chart_id.data.datasets[0].data.push(value);
+  } else {
+      chart_id.data.labels.push(d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
+      chart_id.data.datasets[0].data.push(value);
   }
-};
+  chart_id.update();
+}
 
-var mychart = Chart.Line(ctx, config);
+function changeLED(state, led_id) { // Change LED on message received
+    var state = (state.toString('utf-8') == 'true')
+    var div_id = "ledstatus" + led_id.toString();
+    // console.log('Received data LED for id %s : %s', led_id, state);
 
-setInterval(function() {
-  changeValue();
-}, 200)
+    ledstate[led_id - 1] = state;
 
+    switch (state) {
+        case false: // LED Mati
+            document.getElementById(div_id).style.backgroundColor = "rgb(231, 76, 60)";
+            break;
+        case true: // LED Nyala
+            document.getElementById(div_id).style.backgroundColor = "rgb(46, 204, 113)";
+            break;
+        default: // Data Invalid
+            document.getElementById(div_id).style.backgroundColor = "white";
+    }
+}
+
+function changeLEDButton(led_id) {
+    var div_id = "ledstatus" + led_id;
+    client.publish("topic/" + div_id, (!ledstate[led_id-1]).toString('utf-8'))
+}
+
+// -----------------------------------------------------------------------------
+
+function createChart(canvas, label, color) {
+  var ctx = document.getElementById(canvas).getContext('2d');
+  var config = {
+      type : 'line',
+      data: {
+        labels: [],
+        datasets: [{
+          label: label,
+          borderColor: color,
+          data: [],
+          fill: false,
+        }]
+      },
+      options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          hoverMode: 'index',
+          stacked: false,
+          scales: {
+              yAxes: [{
+                  type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                  display: true,
+                  position: 'left',
+                  gridLines: {
+                      color: '#252525',
+                  },
+              }],
+              xAxes: [{
+                  ticks: {
+                      autoSkip: true,
+                      maxTicksLimit: maxData / 2
+                  },
+                  gridLines: {
+                      color: '#252525',
+                  },
+              }],
+          }
+      }
+  };
+
+  var chart = Chart.Line(ctx, config);
+
+  return chart;
+}
+
+var chart1 = createChart('canvas1', 'Humidity', '#3cba9f');
+var chart2 = createChart('canvas2', 'Temperature', '#c45850')
+var chart3 = createChart('canvas3', 'Brightness', '#6FADCF')
+
+// -----------------------------------------------------------------------------
 
 // Brightness Gauge
 var bar1 = new ldBar("#temp_gauge");
 
 // Hum Gauge
 var opts = {
-  angle: 0.15, // The span of the gauge arc
-  lineWidth: 0.44, // The line thickness
-  radiusScale: 1, // Relative radius
-  pointer: {
-    length: 0.6, // // Relative to gauge radius
-    strokeWidth: 0.035, // The thickness
-    color: '#c45850' // Fill color
-  },
-  limitMax: false, // If false, max value increases automatically if value > maxValue
-  limitMin: false, // If true, the min value of the gauge will be fixed
-  colorStart: '#6FADCF', // Colors
-  colorStop: '#3cba9f', // just experiment with them
-  strokeColor: '#2f2f2f', // to see which ones work best for you
-  generateGradient: true,
-  highDpiSupport: true, // High resolution support
-  staticLabels: {
-    font: "10px sans-serif", // Specifies font
-    labels: [0, 20, 40, 60, 80, 100], // Print labels at these values
-    color: "#fff", // Optional: Label text color
-    fractionDigits: 0 // Optional: Numerical precision. 0=round off.
-  },
+    angle: 0.15, // The span of the gauge arc
+    lineWidth: 0.44, // The line thickness
+    radiusScale: 1, // Relative radius
+    pointer: {
+        length: 0.6, // // Relative to gauge radius
+        strokeWidth: 0.035, // The thickness
+        color: '#c45850' // Fill color
+    },
+    limitMax: false, // If false, max value increases automatically if value > maxValue
+    limitMin: false, // If true, the min value of the gauge will be fixed
+    colorStart: '#6FADCF', // Colors
+    colorStop: '#3cba9f', // just experiment with them
+    strokeColor: '#2f2f2f', // to see which ones work best for you
+    generateGradient: true,
+    highDpiSupport: true, // High resolution support
+    staticLabels: {
+        font: "10px sans-serif", // Specifies font
+        labels: [0, 20, 40, 60, 80, 100], // Print labels at these values
+        color: "#fff", // Optional: Label text color
+        fractionDigits: 0 // Optional: Numerical precision. 0=round off.
+    },
 
 };
 var target = document.getElementById('hum_gauge'); // your canvas element
